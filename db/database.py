f3da6c8a71e1cf0adee690c9286cfd22cf798fdd
KIRT <122153681+KIRT-king@users.noreply.github.com>
2025-03-30 19:07:30 +0300

    Update database.py

--- db/database.py
+++ db/database.py
@@ -1,7 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-with open("connection.txt", "r") as file:
+script_dir = os.path.dirname(os.path.abspath(__file__))
+connection_path = os.path.join(script_dir, "web_interface_for_kirtapp", "connection.txt")
+
+with open(connection_path, "r") as file:
     connection_string = file.read()
 
 engine = create_engine(connection_string)
