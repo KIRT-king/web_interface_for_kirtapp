e4189f994c3d0b608720f6aebce428c7e23ef6b2
KIRT <122153681+KIRT-king@users.noreply.github.com>
2025-03-30 19:08:42 +0300

    Update database.py

--- db/database.py
+++ db/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
-connection_path = os.path.join(script_dir, "web_interface_for_kirtapp", "connection.txt")
+connection_path = os.path.join(script_dir, "connection.txt")
 
 with open(connection_path, "r") as file:
     connection_string = file.read()
